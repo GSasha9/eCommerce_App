3fae5a035128caa151aca902d8c31b8a22278019
Shershneva Kseniya <megacoolksuha@mail.ru>
2025-05-16 16:29:54 +0300

    feat: add temporary request to write token to LS, fix redirect to home page, output to console written tokens

--- ecommerce-application/src/services/sdk/token.ts
+++ ecommerce-application/src/services/sdk/token.ts
@@ -5,6 +5,8 @@ export const tokenCache = (key: string): TokenCache => ({
   get: (): TokenStore => {
     const token = localStorage.getItem(key);
 
+    console.log('get', key, token);
+
     if (token) {
       const parsed: unknown = JSON.parse(token);
 
@@ -15,5 +17,9 @@ export const tokenCache = (key: string): TokenCache => ({
 
     return { expirationTime: 0, token: '' };
   },
-  set: (token): void => localStorage.setItem(key, JSON.stringify(token)),
+  set: (token): void => {
+    console.log('set', key, token);
+
+    localStorage.setItem(key, JSON.stringify(token));
+  },
 });
